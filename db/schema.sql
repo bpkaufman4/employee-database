f1761306907b56e3c5be75cb35bff5768a721d39
Brian Kaufman <bpkaufman4@gmail.com>
2021-09-20 15:00:35 -0500

    added sql files

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Brian', 'Kaufman', 3541, 1),
+  ('Emily', 'Kaufman', 0331, 2),
+  ('Christian', 'Kaufman', 5505, 1),
+  ('Olivia', 'Kaufman', 7894, 2),
+  ('Derek', 'Engel', 8844, NULL),
+  ('Andrew', 'McClure', 1234, NULL);
